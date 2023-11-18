223d3389c9b9d7cfc654b5b55d9bbe64b483994a
Webkat <webket49@gmail.com>
2023-11-18 19:02:26 +0000

    javascript added

--- cs-dpt.js
+++ cs-dpt.js
@@ -10,3 +10,21 @@ document.addEventListener("DOMContentLoaded", function() {
 
    
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    var backToTopBtn = document.getElementById("back-to-top-btn");
+
+    window.addEventListener("scroll", function() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            backToTopBtn.style.display = "block";
+        } else {
+            backToTopBtn.style.display = "none";
+        }
+    });
+
+    backToTopBtn.addEventListener("click", function() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    });
+});
